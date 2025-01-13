4c5b07e0fa4aef998949b23d9b8eba0ac1cfea6f
Nihad <mncnihad@gmail.com>
2025-01-13 08:08:43 +0530

    created files and folder

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import NavBar from "./Components/NavBar/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
